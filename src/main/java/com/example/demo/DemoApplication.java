1a6d0022916ef543555c3d2937f3855cb809384c
xixiangyu <610013474@qq.com>
2025-04-22 10:39:42 +0800

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+@RestController
+class DemoController {
+    @GetMapping("/")
+    public String hello() {
+        LocalDateTime now = LocalDateTime.now();
+        return "Hello World!" + now;
+    }
+}
