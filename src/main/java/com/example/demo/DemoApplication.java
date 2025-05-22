c740631cc11407452cf5e404e69c322d1a55c11b
xixiangyu <610013474@qq.com>
2025-05-22 11:23:51 +0800

    添加日志

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +19,14 @@ public class DemoApplication {
 }
 
 @RestController
+@Slf4j
 class DemoController {
     @Timed(value = "main_page_request_duration", description = "Time taken to return main page", histogram = true)
     @GetMapping("/")
     public String hello() {
         LocalDateTime now = LocalDateTime.now();
+        log.info("Request received at {}", now);
+        log.error("Error at {}", now);
         return "Hello World!" + now;
     }
 }
